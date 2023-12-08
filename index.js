2bcff5ee97181c6c1493533a7c52d26851c05f8f
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-12-08 10:13:05 +0530

    ui part done for todo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("welcome to my todo app");
 
 let todoDataSection = document.getElementById("todo-data");
 
-function addTodo() {
+function addTodo(todoData) {
   let rowDiv = document.createElement("div");
   let todoItem = document.createElement("div");
   let todoNumber = document.createElement("div");
@@ -13,6 +13,47 @@ function addTodo() {
   let deleteButton = document.createElement("button");
   let finishedButton = document.createElement("button");
   let hr = document.createElement("hr");
+  /*
+  <div class="row">
+                    <div class="todo-item d-flex flex-row justify-content-between align-items-center ">
+                        <div class="todo-no">1.</div>
+                        <div class="todo-detail text-muted">Complete the action inside the todo items</div>
+                        <div class="todo-status text-muted">In Progress</div>
+                        <div class="todo-actions d-flex flex-row justify-content-start gap-2">
+                            <button class="btn btn-danger ">Delete</button>
+                            <button class="btn btn-success ">Finished</button>
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+                */
+  //adding classes
+  rowDiv.classList.add("row");
+  todoItem.classList.add(
+    "todo-item",
+    "d-flex",
+    "flex-row",
+    "justify-content-between",
+    "align-items-center"
+  );
+  todoNumber.classList.add("todo-no");
+  todoDetail.classList.add("todo-detail", "text-muted");
+  todoStatus.classList.add("todo-status", "text-muted");
+  todoActions.classList.add(
+    "todo-actions",
+    "d-flex",
+    "flex-row",
+    "justify-content-start",
+    "gap-2"
+  );
+  deleteButton.classList.add("btn", "btn-danger");
+  finishedButton.classList.add("btn", "btn-success");
+
+  todoNumber.textContent = "1.";
+  todoDetail.textContent = todoData; //sets the todo  text sent from the input element
+  todoStatus.textContent = "In progress";
+  deleteButton.textContent = "Delete";
+  finishedButton.textContent = "Finished";
 
   todoActions.appendChild(deleteButton);
   todoActions.appendChild(finishedButton);
@@ -23,6 +64,8 @@ function addTodo() {
   todoItem.appendChild(todoActions);
   rowDiv.appendChild(todoItem);
   rowDiv.appendChild(hr);
+
+  todoDataSection.appendChild(rowDiv);
 }
 
 //references for event and event listners
